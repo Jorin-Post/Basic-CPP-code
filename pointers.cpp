dc52e8270676dc54ad66c8570b173d8a3e24d6d0
Jorin-Post <jorin101@hotmail.nl>
2021-07-08 17:30:57 +0200

    V2

--- pointers.cpp
+++ pointers.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 template<typename T>
-void prin(T *p){
-    cout << *p << endl;
+void prin(T *p, T s){
+    for(T i=0; i < (s/4); i++){
+        cout << *(p+i) << '\t';
+    }
 }
 int main(){
-    int a = 10, *p = &a;
-    *p = 15;
-    *p+1;
-    prin(p);
+    int a[2][10]={{0,1,2,3,4,5,6,7,8,9},{10,11,12,13,14,15,16,17,18,19}}; 
+    int *p = &a[0][0];
+    int s = sizeof(a);
+    prin(p, s);
 }
